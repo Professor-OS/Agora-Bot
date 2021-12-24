4300de2d1a6bc6c403154b5d76806132b32cd68f
Professor Agora <agoraprofessor@gmail.com>
2021-12-24 08:58:49 +0530

    Update misc.py

--- assistant/manager/misc.py
+++ assistant/manager/misc.py
@@ -1,18 +1,18 @@
-# Ultroid - UserBot
-# Copyright (C) 2021 TeamUltroid
+# Agora - UserBot
+# Copyright (C) 2021 Professor-OS
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# This file is a part of < https://github.com/Professor-OS/Agora-Bot/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# <https://www.github.com/Professor-OS/Agora-Bot/blob/main/LICENSE/>.
 
 
 import aiohttp
 import random
-from pyUltroid.dB import DEVLIST
+from pyAgora.dB import SHAAN
 from . import *
 
 
-@ultroid_cmd(pattern="decide", type="manager")
+@agora_cmd(pattern="decide", type="manager")
 async def dheh(e):
     text = ["Yes", "NoU", "Maybe", "IDK"]
     text = random.choice(text)
@@ -21,7 +21,7 @@ async def dheh(e):
         ri = e.reply_to_msg_id
     await e.client.send_message(e.chat_id, text, reply_to=ri)
 
-@ultroid_cmd(pattern="echo ?(.*)", type=["manager"])
+@agora_cmd(pattern="echo ?(.*)", type=["manager"])
 async def oqha(e):
     match = e.pattern_match.group(1)
     if match:
@@ -35,7 +35,7 @@ async def oqha(e):
     await e.client.send_message(e.chat_id, text, reply_to=reply_to)
 
 
-@ultroid_cmd(pattern="kickme$", type=["manager"], allow_all=True)
+@agora_cmd(pattern="kickme$", type=["manager"], allow_all=True)
 async def doit(e):
     if e.sender_id in DEVLIST:
         return await eod(e, "`I will Not Kick You, my Developer..`")
@@ -46,7 +46,7 @@ async def doit(e):
     await eor(e, "Yes, You are right, get out.", time=5)
 
 
-@ultroid_cmd(pattern="joke$", type=["manager"])
+@agora_cmd(pattern="joke$", type=["manager"])
 async def do_joke(e):
     e = await e.get_reply_message() if e.is_reply else e
     link = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
